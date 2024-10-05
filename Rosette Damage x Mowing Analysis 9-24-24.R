f56615636418ae9fd166afca269dda164a8e1963
emmavdheide <erv5124@psu.edu>
2024-10-05 17:53:41 -0400

    Dump of progress since the last time I made a commit

--- Rosette Damage x Mowing Analysis 9-24-24.R
+++ Rosette Damage x Mowing Analysis 9-24-24.R
@@ -1,6 +1,5 @@
 #set working directory
-#setwd("~/Desktop") #for Olivia
-setwd("~/Grad School/Penn State/Project Information/Thistle Nursery for Summer 2024") #for Emma
+setwd("~/Desktop")
 
 #load data
 dat <- read.csv("ManuscriptData1.csv")
@@ -9,46 +8,94 @@ dat <- read.csv("ManuscriptData1.csv")
 dat$Block <- as.factor(dat$Block)
 dat$SubBlock <- as.factor(dat$SubBlock)
 dat$Treatment <- as.factor(dat$Treatment)
+dat$RosDam <- as.factor(dat$RosDam)
+dat$EarlyMow <- as.factor(dat$EarlyMow)
+dat$LateMow <- as.factor(dat$LateMow)
+dat$SurviveJuly5 <- as.factor(dat$SurviveJuly5)
 
-#fit flowering date model
-fitfd <- lm(FloweringDate~Treatment, data = dat)
-summary(fitfd)
-
-#ANOVA
-anova(fitfd)
-
-#residuals
-plot(fitfd)
-
-
-#fit height at flowering model
-fithf <- lm(HeightatFlowering~Treatment, data = dat)
-summary(fithf)
-
-#ANOVA
-anova(fithf)
-
-#residuals- look worse?idk
-plot(fithf)
-
-#install.packages("lme4")
-#install.packages("Matrix")
+#library all packages
 library(lme4)
+library(glmmTMB)
+library(DHARMa)
+library(emmeans)
+library(ggplot2)
+
 fit1 <- lmer(HeightatFlowering ~ Treatment + (1|Block), data=dat)
 fit2 <- lmer(HeightatFlowering ~ Treatment + FloweringDate + (1|Block), data = dat)
-
-
 
 fit3 <- glmer(MaxCapitula ~ Treatment + (1|Block), data = dat, family = "poisson")
 fit4 <- lmer(MaxCapitula ~ Treatment + (1|Block), data = dat)
 
+fit5 <- glmmTMB(MaxCapitula ~ Treatment + LLLMay2 + (1|Block), data = dat, family = "poisson")
+summary(fit5)
 
-install.packages("DHARMa")
-library(DHARMa)
+sim.fit5 <- simulateResiduals(fit5)
+plot(sim.fit5)
 
-sim.fit3 <- simulateResiduals(fit3)
-plot(sim.fit3)
+emmMaxCap <- emmeans(fit5, specs=pairwise~Treatment, type="response")
+emmMaxCap
 
-sim.fit4 <- simulateResiduals(fit4)
-plot(sim.fit4)
-#is there a diff between residuals 3 and 4? family poisson?
+MaxCap<-as.data.frame(emmMaxCap[1])
+
+MaxCapPlot<-ggplot(MaxCap,aes(y=emmeans.rate, x=emmeans.Treatment))+
+  geom_bar(stat = "identity")+
+  theme(panel.background = element_blank(), panel.border = element_rect(color = "black",fill=NA, size=1))+
+  labs(x="Treatment", y="Capitula Production")+
+  ylim(0, 12)+
+  ggtitle("Maximum Number of Capitula Present on Plant")+
+  geom_errorbar(aes(x=emmeans.Treatment, ymin=emmeans.asymp.LCL, ymax=emmeans.asymp.UCL))
+MaxCapPlot
+
+#change order of magnitude on date numbers
+dat$SmallDate <- dat$FloweringDate/1000
+#issue with magnitude, use transformed version smalldate!
+
+#this is bad
+fit6 <- glmmTMB(FloweringDate ~ Treatment + (1|Block), data = dat, family = "nbinom2")
+summary(fit6)
+sim.fit6 <- simulateResiduals(fit6)
+plot(sim.fit6)
+
+#this is better
+fit7 <- glmmTMB(SmallDate ~ Treatment + (1|Block), data = dat, family = "gaussian")
+summary(fit7)
+sim.fit7 <- simulateResiduals(fit7)
+plot(sim.fit7)
+
+
+#height at flowering by treatment
+fit8 <- glmmTMB(HeightatFlowering ~ RosDam*EarlyMow*LateMow + (1|Block) + LLLMay2, data = dat, family = "gaussian")
+summary(fit8)
+
+sim.fit8 <- simulateResiduals(fit8)
+plot(sim.fit8)
+
+emmHtFl <- emmeans(fit8, specs=pairwise~RosDam*EarlyMow*LateMow, type="response")
+emmHtFl
+
+fit8B <- glmmTMB(HeightatFlowering ~ Treatment + (1|Block) + LLLMay2, data = dat, family = "gaussian")
+summary(fit8B)
+
+emmHtFl2 <- emmeans(fit8B, specs=pairwise~Treatment, type="response")
+emmHtFl2
+
+HtFl<-as.data.frame(emmHtFl2[1])
+
+HtFlPlot<-ggplot(HtFl,aes(y=emmeans.emmean, x=emmeans.Treatment))+
+  geom_bar(stat = "identity")+
+  theme(panel.background = element_blank(), panel.border = element_rect(color = "black",fill=NA, size=1))+
+  labs(x="Treatment", y="Height at Flowering")+
+  ylim(0, 120)+
+  ggtitle("Height at Flowering")+
+  geom_errorbar(aes(x=emmeans.Treatment, ymin=emmeans.lower.CL, ymax=emmeans.upper.CL))
+HtFlPlot
+
+#survivorship by treatment and LLL
+#including LLL gives NaN in summary
+fit9 <- glmmTMB(SurviveJuly5 ~ Treatment + (1|Block), data = dat, family = "binomial")
+summary(fit9)
+sim.fit9 <- simulateResiduals(fit9)
+plot(sim.fit9)
+#rosette damage and late mow give NaN
+fit10 <- glmmTMB(SurviveJuly5 ~ EarlyMow + (1|Block), data = dat, family = "binomial")
+summary(fit10)
